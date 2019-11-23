78583d9ecffd39dc27f86614a64478fac54228fb
hellonimesh <nimeshkumarsoni@yahoo.com>
2019-11-23 21:38:25 +0000

    Adding Consol output to pass the number of counts as an output

--- PetStore.Tests/PetTests.cs
+++ PetStore.Tests/PetTests.cs
@@ -5,17 +5,27 @@ namespace PetStore.Tests
 {
     public class PetTests
     {
+
+        PetStoreAPIClient petStoreAPIClient;
+
         [SetUp]
         public void Setup()
         {
+             petStoreAPIClient = new PetStoreAPIClient();
         }
 
         [Test]
         public void Assert_Available_Pets_With_Doggie_Name_Count()
-        {
-            PetStoreAPIClient petStoreAPIClient = new PetStoreAPIClient();
-            int petCount = petStoreAPIClient.GetPetCount("available", "doggie").Result;
+        { 
+          int petCount = petStoreAPIClient.GetPetCount("available", "doggie").Result;
+            System.Console.WriteLine($"The number of pets available are =  {petCount}");
             Assert.NotZero(petCount);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            petStoreAPIClient = null;
         }
     }
 }
